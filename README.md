5cfe5e86f97a08555656473b6f42214aa706f2fe
leiwu123 <jobspecific@hotmail.com>
2018-08-06 07:50:36 +0800

    Added btc-price model, added btc price chart, added routing

--- README.md
+++
@@ -1,2 +0,0 @@
-# CryptoTop100
-Angular 5 Crypto Top 100 API &amp; Charting Project
--- crypto/src/app/app.module.ts
+++ crypto/src/app/app.module.ts
@@ -3,25 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
 
+import { ChartModule } from 'angular2-chartjs';
+
 import { AppComponent } from './app.component';
 import { CryptoService } from '../services/crypto.service';
 import { CryptoTableComponent } from '../crypto-table/crypto-table.component';
 import { NotFoundComponent } from '../not-found/not-found.component';
+import { BitcoinStatsComponent } from '../bitcoin-stats/bitcoin-stats.component';
 
 const appRoutes: Routes = [
   { path: '', component: CryptoTableComponent },
-  // { path: 'bitcoinStats', component: BitcoinStatsComponent },
-  { path: '**', component: NotFoundComponent }
+  { path: 'bitcoinstats', component: BitcoinStatsComponent },
+  // { path: '**', component: NotFoundComponent }
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
     CryptoTableComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    BitcoinStatsComponent
   ],
   imports: [
     BrowserModule,
+    ChartModule,
     HttpClientModule,
     RouterModule.forRoot(appRoutes)
   ],
