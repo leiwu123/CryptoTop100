d32a352fde994ff73c47f41071a08ddd1b0af388
leiwu123 <jobspecific@hotmail.com>
2018-08-06 15:07:38 +0800

    Added show growth model

--- crypto/src/crypto-filter/crypto-filter.component.ts
+++ crypto/src/crypto-filter/crypto-filter.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "../../node_modules/@angular/core";
+import { Component, Input } from "../../node_modules/@angular/core";
+import { CryptoCurrency } from '../models/crypto-currency.class';
 
 @Component({
   selector: 'crypto-filter',
@@ -7,5 +8,21 @@ import { Component } from "../../node_modules/@angular/core";
 })
 
 export class CryptoFilterComponent {
+  @Input() public cryptos: CryptoCurrency[];
+  public filteredCryptos: CryptoCurrency[];
+  public percentChange: string = 'All';
 
+  public percentChangeFilter(): void {
+    console.log(this.cryptos);
+    console.log(this.percentChange);
+    this.filteredCryptos = this.cryptos.filter((crypto: CryptoCurrency) => {
+        if (this.percentChange === 'Positive') {
+          return crypto.percent_change_24h >= 0;
+        } else if (this.percentChange === 'Negative') {
+          return crypto.percent_change_24h < 0;
+        }
+        return crypto;
+    });
+    console.log(this.filteredCryptos);
+  }
 }
